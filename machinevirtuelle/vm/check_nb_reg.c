ade3df599a6f76d163620eea2656a4c0ff75a66e
Karim AHANTAR <kahantar@e3r13p13.42.fr>
2017-06-27 20:37:21 +0200

    seconde end

--- machinevirtuelle/vm/check_nb_reg.c
+++ machinevirtuelle/vm/check_nb_reg.c
@@ -19,3 +19,58 @@ int		check_nb_reg(unsigned int nb)
 	else
 		return (0);
 }
+
+void	cpyint(unsigned char *ram, unsigned char *src, int pc)
+{
+	ram[pc] = src[0];
+	ram[mask_pc(pc, 1)] = src[1];
+	ram[mask_pc(pc, 2)] = src[2];
+	ram[mask_pc(pc, 3)] = src[3];
+}
+
+int			free_process(t_process **process)
+{
+	t_process *cp;
+
+	cp = (*process);
+	while (cp)
+	{
+		cp = (*process)->next;
+		free(*process);
+		*process = cp;
+	}
+	return (0);
+}
+
+unsigned int	*init_number_player(int nb, t_path *path)
+{
+	unsigned int	*ret;
+	int				x;
+
+	x = 1;
+	ret = init_tab(nb + 1);
+	ret[0] = nb;
+	while (path)
+	{
+		ret[x] = path->nb;
+		x++;
+		path = path->next;
+	}
+	return (ret);
+}
+
+unsigned int	*init_tab(int x)
+{
+	unsigned int	*tab;
+	int				i;
+
+	i = 0;
+	if (!(tab = (unsigned int*)malloc(sizeof(unsigned int) * x)))
+		return (0);
+	while (i < x)
+	{
+		tab[i] = 0;
+		i++;
+	}
+	return (tab);
+}
--- machinevirtuelle/vm/load_value.c
+++ machinevirtuelle/vm/load_value.c
@@ -37,3 +37,57 @@ unsigned int *type, unsigned int *val)
 	}
 	return (ret);
 }
+
+unsigned int	loadint(unsigned char *ram, int pc)
+{
+	unsigned int ret;
+
+	ret = (ram[pc] << 24)
+		| ((ram[pc + 1] << 16) & 0x00ffffff)
+		| ((ram[pc + 2] << 8) & 0x0000ffff)
+		| (ram[pc + 3] & 0x000000ff);
+	return (ret);
+}
+
+int		mask_pc(int nb, int i)
+{
+	int ret;
+
+	ret = (nb + i) % (MEM_SIZE - 1);
+	return (ret);
+}
+
+int	print_ram(unsigned char *ram)
+{
+	int i;
+	int x;
+
+	i = 0;
+	while (i < MEM_SIZE)
+	{
+		x = 0;
+		while (x < 32)
+		{
+			if (ram[i] < 16)
+				ft_printf("0%hhx ", ram[i]);
+			else
+				ft_printf("%hhx ", ram[i]);
+			x++;
+			i++;
+		}
+		ft_printf("\n");
+	}
+	return (1);
+}
+
+unsigned char	*reverseint(unsigned int nb)
+{
+	unsigned char *ret;
+
+	ret = (unsigned char *)ft_strnew(4);
+	ret[0] = (nb & 0xff000000) >> 24;
+	ret[1] = (nb & 0x00ff0000) >> 16;
+	ret[2] = (nb & 0x0000ff00) >> 8;
+	ret[3] = (nb & 0x000000ff);
+	return (ret);
+}
